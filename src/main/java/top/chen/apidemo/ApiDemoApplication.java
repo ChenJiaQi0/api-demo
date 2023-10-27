49c30dd8862714d21199544b21c91cae9a06b170
TONGTONGDESHIJIE <1090060206@qq.com>
2023-10-27 13:30:29 +0800

    添加跨域注解

--- src/main/java/top/chen/apidemo/ApiDemoApplication.java
+++ src/main/java/top/chen/apidemo/ApiDemoApplication.java
@@ -2,6 +2,7 @@ package top.chen.apidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin
 public class ApiDemoApplication {
 
     public static void main(String[] args) {
