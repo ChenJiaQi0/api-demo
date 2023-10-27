48c4f346c4a4f45e238debe933ce5d3260e443bf
TONGTONGDESHIJIE <1090060206@qq.com>
2023-10-27 11:02:08 +0800

    api-demo构建镜像

---
+++ src/test/java/top/chen/apidemo/ApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.chen.apidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
